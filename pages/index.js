d1701a6f9e12f8906c7c269ca844cba3997cec5b
KaiPereira <88850028+KaiPereira@users.noreply.github.com>
2022-08-05 17:25:38 -0700

    Got rid of loading animation

--- pages/index.js
+++ pages/index.js
@@ -22,21 +22,6 @@ export default function Home({ posts, isFirstMount }) {
       changeCursorVariant("default")
   }
 
-
-  const pageChangeTransition = {
-    initial: {
-      height: "100vh",
-      top: 0
-    },
-    animate: {
-      height: 0,
-      transition: {
-        duration: 0.85,
-        ease: [0.87, 0, 0.13, 1],
-      },
-    }
-  }
-
   const animationConfiguration = {
       initial: { opacity: 0 },
       animate: { opacity: 1 },
@@ -45,16 +30,6 @@ export default function Home({ posts, isFirstMount }) {
 
   return (
     <motion.div variants={!isFirstMount && animationConfiguration} initial="initial" animate="animate" exit="exit" transition={{ duration: 0.5 }} className="opacityPageTransition">
-      { isFirstMount &&
-      <div className="pageTransitionContainer">
-        <motion.div 
-          className="pageTransition"
-          initial="initial"
-          animate="animate"
-          variants={pageChangeTransition}
-        />
-      </div>
-      }
       <Nav 
         cursorEnter={cursorEnter}
         cursorLeave={cursorLeave}
