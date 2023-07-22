9cfba116e5927156b97fd293b406ccc876171b7e
Kai Pereira <88850028+KaiPereira@users.noreply.github.com>
2023-07-22 16:01:50 +0000

    fixed my stupid robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://kaipereira.me',
-    generateRobotsTxt: true
+    siteUrl: process.env.SITE_URL || 'https://kaipereira.me'
 }
